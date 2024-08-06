7d47b6b3afef88231580c564f79609bf59e9854c
Camila Arce <arce.b.camila@gmail.com>
2024-08-06 14:26:37 -0300

    refactor: :fire: agregar paquete dotenv
    credenciales irán en el archivo .env para evitar exponerlas

--- db-connection.js
+++ db-connection.js
@@ -1,11 +1,11 @@
+require('dotenv').config();
 var mysql = require('mysql2');
 
-// completar con los datos de tu base de datos
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'fororecetas' 
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
